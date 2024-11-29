695fc3b4f73adaefb7ed48c8f123b33ff175b8ec
Joshua Lavaud <j.lavaud501@gmail.com>
2024-11-29 18:03:06 +0100

    Ajout JSON pour les formations

--- src/components/Formation/index.jsx
+++ src/components/Formation/index.jsx
@@ -1,17 +1,25 @@
 import './formation.scss'
+import bdd from '../../bdd/formations.json'
 
 function Formation() {
   return (
     <div className="formation module2">
       <h2>Formations</h2>
-      <div className="formation__card">
-        <h3>Développeur WEB</h3>
-        <ul>
-          <li>Organisme : OpenClassrooms</li>
-          <li>Réalisée en 2024</li>
-          <li>Programmation de site Web côté front et back</li>
-        </ul>
-      </div>
+      {bdd.map((formation) => (
+        <div className="formation__card">
+          <h3>{formation.titre}</h3>
+          <ul>
+            <li>
+              Diplome de niveau {formation.niveau} (Bac {formation.niveauBac})
+            </li>
+            <li>Organisme : {formation.organisme}</li>
+            <li>Réalisée en {formation.realiseAnnee}</li>
+            <li>Durée : {formation.duree}</li>
+            <li>{formation.desc}</li>
+            <li>{formation.distanceOuPresence}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   )
 }
