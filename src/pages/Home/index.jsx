586e4642158f4a7eaea7685916170142cfc89a5e
Joshua Lavaud <j.lavaud501@gmail.com>
2025-02-07 11:42:36 +0100

    Rework of project

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,11 @@
-import './App.scss'
-import Navigation from '../../components/Navigation'
-import Banniere from '../../components/Banniere'
-import Skills from '../../components/Skills'
-import Apropos from '../../components/Apropos'
-import Projets from '../../components/Projets'
-import Formation from '../../components/Formation'
-
-function App() {
+function Homme() {
   return (
     <div className="App">
-      <header>
-        <Banniere />
-        <Navigation />
-      </header>
-      <main>
-        <Apropos />
-        <Skills />
-        <Formation />
-        <Projets />
-      </main>
+      <header></header>
+      <main></main>
       <footer></footer>
     </div>
   )
 }
 
-//<img src={logo} className="App-logo" alt="logo" />
-export default App
+export default Homme
